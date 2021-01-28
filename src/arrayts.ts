f15c97b9489679e996eea9d31eea12002b02c7e3
leguass7 <leandro.sbrissa@hotmail.com>
2021-01-28 17:24:50 -0300

    test: lint-staged

--- src/arrayts.ts
+++ src/arrayts.ts
@@ -1,9 +1,9 @@
 /**
- * @description
- * bla bla
+ * Forces the return of an array
+ * @function forceArray
  * @example
  * forceArray('a') // ['a']
- *
+ * forceArray(['a']) // ['a']
  */
 export function forceArray<T>(value: T | T[]): T[] {
   return !Array.isArray(value) ? [value] : value || []
