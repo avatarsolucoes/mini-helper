b6e5b6766b7a3e5b4b975ee2f51d74e760113d1e
leguass7 <leandro.sbrissa@hotmail.com>
2021-02-02 11:22:20 -0300

    chore(release): removed node deps

--- dist/image.d.ts
+++ dist/image.d.ts
@@ -1,10 +1,4 @@
 /**
- * @function convertSvgToPng
- * @param {String} source
- * @param {String} dest
- */
-export function convertSvgToPng(source: string, dest: string): Promise<any>;
-/**
  * @function toBase64Img
  * @param {String} filePath
  */
